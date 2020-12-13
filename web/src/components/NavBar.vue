8c7fbc4032df5da853736052778d2f79c1d2afec
Corey <mechtrondev@gmail.com>
2020-12-12 16:51:06 -0800

    Create VueJS app and integrate BootstrapVue

---
+++ web/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <b-nav align="center">
+    <b-nav-item active>Home</b-nav-item>
+    <b-nav-item>Top 10</b-nav-item>
+    <b-nav-item>Login</b-nav-item>
+  </b-nav>
+</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
