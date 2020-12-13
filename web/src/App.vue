1d6c656c3f8e9697749af943f528de7d83389a37
Corey <mechtrondev@gmail.com>
2020-12-12 16:51:06 -0800

    Create VueJS app and integrate BootstrapVue

---
+++ web/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <Home msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 1px;
+}
+</style>
