6c92f870b4a7a7aa5eaef05f2eed3bf7308b93d6
Corey <mechtrondev@gmail.com>
2020-12-13 23:24:22 -0800

    Add About page content

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,21 +1,34 @@
-<template>
-  <div>
-    <h1>About</h1>
+<template lang="html">
+  <div class="content-centered">
+    <h3>Tech Stack</h3>
+    <div class="card">
+      <div class="card-header">Frontend</div>
+        <b-link href="https://vuejs.org" target="_blank">VueJS</b-link>
+        <b-link href="https://bootstrap-vue.org" target="_blank">BootstrapVue</b-link>
+        <b-link href="https://router.vuejs.org" target="_blank">Vue Router</b-link>
+        <b-link href="https://vuex.vuejs.org" target="_blank">Vuex</b-link>
+        <b-link href="https://github.com/imcvampire/vue-axios" target="_blank">Axios</b-link>
+    </div>
+    <br/>
+    <div class="card">
+      <div class="card-header">Backend</div>
+        <b-link href="https://www.python.org" target="_blank">Python</b-link>
+        <b-link href="https://www.django-rest-framework.org" target="_blank">Django REST Framework</b-link>
+        <b-link href="https://www.intenct.nl/projects/django-allauth/" target="_blank">django-allauth</b-link>
+        <b-link href="https://www.sqlite.org/index.html" target="_blank">SQLite</b-link>
+        <b-link href="https://swagger.io/tools/swagger-ui/" target="_blank">Swagger UI</b-link>
+    </div>
+    <br/>
+    <div class="card">
+      <div class="card-header">DevOps</div>
+        <b-link href="https://www.docker.com" target="_blank">Docker</b-link>
+        <b-link href="https://kubernetes.io" target="_blank">Kubernetes</b-link>
+        <b-link href="https://aws.amazon.com/eks/" target="_blank">EKS</b-link>
+        <b-link href="https://helm.sh" target="_blank">Helm</b-link>
+        <b-link href="https://www.getambassador.io" target="_blank">Ambassador</b-link>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      example1: 'test'
-    }
-  },
-  props: {
-    example2: String
-  }
-}
-</script>
-
-<style scoped>
+<style scoped lang="scss">
 </style>
