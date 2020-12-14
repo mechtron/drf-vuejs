84d59a64ba82b9a4e663188e70dd06c83581fc69
Corey <mechtrondev@gmail.com>
2020-12-13 22:22:41 -0800

    WIP: login via Google OAuth

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -5,12 +5,36 @@
     <b-nav-item to="/posts">Posts</b-nav-item>
     <b-nav-item to="/hof">HOF</b-nav-item>
     <b-nav-item to="/about">About</b-nav-item>
-    <b-nav-item to="/login">Login</b-nav-item>
+    <b-nav-item @click="oAuthLogin('google')">Login</b-nav-item>
   </b-nav>
 </div>
 </template>
 
 <script>
+// import { store, mutations } from "./store";
+import oauth from '../oauth';
+export default {
+  // computed: {
+  //   newsletters() {
+  //     return store.newsletters;
+  //   }
+  // },
+  data() {
+    return {
+      show: true
+    }
+  },
+  methods: {
+    // setNewsletter: mutations.setNewsletter,
+    oAuthLogin(provider) {
+      oauth.login(provider);
+    }
+  }
+  // ,
+  // mounted() {
+  //   this.updateFormFields()
+  // }
+}
 </script>
 
 <style scoped>
