d88e6ba1972d7b5fb3da1f1f90c58ed76df9f50d
Corey <mechtrondev@gmail.com>
2020-12-14 01:15:26 -0800

    Put About page contents in 12col grid

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,33 +1,39 @@
 <template lang="html">
-  <div class="content-centered">
-    <h3>Tech Stack</h3>
-    <div class="card">
-      <div class="card-header">Frontend</div>
-        <b-link href="https://vuejs.org" target="_blank">VueJS</b-link>
-        <b-link href="https://bootstrap-vue.org" target="_blank">BootstrapVue</b-link>
-        <b-link href="https://router.vuejs.org" target="_blank">Vue Router</b-link>
-        <b-link href="https://vuex.vuejs.org" target="_blank">Vuex</b-link>
-        <b-link href="https://github.com/imcvampire/vue-axios" target="_blank">Axios</b-link>
-    </div>
-    <br/>
-    <div class="card">
-      <div class="card-header">Backend</div>
-        <b-link href="https://www.python.org" target="_blank">Python</b-link>
-        <b-link href="https://www.django-rest-framework.org" target="_blank">Django REST Framework</b-link>
-        <b-link href="https://www.intenct.nl/projects/django-allauth/" target="_blank">django-allauth</b-link>
-        <b-link href="https://www.sqlite.org/index.html" target="_blank">SQLite</b-link>
-        <b-link href="https://swagger.io/tools/swagger-ui/" target="_blank">Swagger UI</b-link>
-    </div>
-    <br/>
-    <div class="card">
-      <div class="card-header">DevOps</div>
-        <b-link href="https://www.docker.com" target="_blank">Docker</b-link>
-        <b-link href="https://kubernetes.io" target="_blank">Kubernetes</b-link>
-        <b-link href="https://aws.amazon.com/eks/" target="_blank">EKS</b-link>
-        <b-link href="https://helm.sh" target="_blank">Helm</b-link>
-        <b-link href="https://www.getambassador.io" target="_blank">Ambassador</b-link>
-    </div>
-  </div>
+<b-container class="bv-example-row">
+      <b-row>
+        <b-col></b-col>
+        <b-col cols="8"> 
+          <h3>Tech Stack</h3>
+          <div class="card">
+            <div class="card-header">Frontend</div>
+              <b-link href="https://vuejs.org" target="_blank">VueJS</b-link>
+              <b-link href="https://bootstrap-vue.org" target="_blank">BootstrapVue</b-link>
+              <b-link href="https://router.vuejs.org" target="_blank">Vue Router</b-link>
+              <b-link href="https://vuex.vuejs.org" target="_blank">Vuex</b-link>
+              <b-link href="https://github.com/imcvampire/vue-axios" target="_blank">Axios</b-link>
+          </div>
+          <br/>
+          <div class="card">
+            <div class="card-header">Backend</div>
+              <b-link href="https://www.python.org" target="_blank">Python</b-link>
+              <b-link href="https://www.django-rest-framework.org" target="_blank">Django REST Framework</b-link>
+              <b-link href="https://www.intenct.nl/projects/django-allauth/" target="_blank">django-allauth</b-link>
+              <b-link href="https://www.sqlite.org/index.html" target="_blank">SQLite</b-link>
+              <b-link href="https://swagger.io/tools/swagger-ui/" target="_blank">Swagger UI</b-link>
+          </div>
+          <br/>
+          <div class="card">
+            <div class="card-header">DevOps</div>
+              <b-link href="https://www.docker.com" target="_blank">Docker</b-link>
+              <b-link href="https://kubernetes.io" target="_blank">Kubernetes</b-link>
+              <b-link href="https://aws.amazon.com/eks/" target="_blank">EKS</b-link>
+              <b-link href="https://helm.sh" target="_blank">Helm</b-link>
+              <b-link href="https://www.getambassador.io" target="_blank">Ambassador</b-link>
+          </div>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
 </template>
 
 <style scoped lang="scss">
