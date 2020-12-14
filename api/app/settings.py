9c2a8a591c15e95b64893887e3da54e37ae40fd9
Corey <mechtrondev@gmail.com>
2020-12-13 22:09:41 -0800

    Tweak CORS settings

--- api/app/settings.py
+++ api/app/settings.py
@@ -54,17 +54,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8080',
+    'http:///127.0.0.1:8000',
     'http://127.0.0.1:8080',
+    'https://accounts.google.com',
 ]
 
 ROOT_URLCONF = 'app.urls'
