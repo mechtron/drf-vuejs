8721304a09a9ca2ae5c29c35eb25d1d6ee63df5a
Corey <mechtrondev@gmail.com>
2020-12-18 22:50:35 -0800

    Refactor

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -5,8 +5,8 @@
     <b-nav-item to="/posts">Posts</b-nav-item>
     <b-nav-item to="/hof">HOF</b-nav-item>
     <b-nav-item to="/about">About</b-nav-item>
-    <b-nav-item @click="login('google')" v-if="!$store.getters.user.isLoggedIn">Login</b-nav-item>
-    <b-nav-item @click="logout('google')" v-if="$store.getters.user.isLoggedIn">Logout</b-nav-item>
+    <b-nav-item @click="logout('google')" v-if="user.isLoggedIn">Logout</b-nav-item>
+    <b-nav-item @click="login('google')" v-else>Login</b-nav-item>
   </b-nav>
 </div>
 </template>
@@ -28,7 +28,7 @@ export default {
       'logout'
     ]),
     detectSession() {
-      if (self.user != null && self.user.token != '' && self.user.username != '') {
+      if (self.user != null && self.user.token != '') {
         console.log("Previous session detected..");
         self.getSelf(self.user.token);
       }
