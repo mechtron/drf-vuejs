45af336c59ba56c02a738b6dff5d8293bc0ecac1
Corey <mechtrondev@gmail.com>
2020-12-26 22:00:45 -0800

    Parameterize SECRET_KEY and DEBUG mode. Bind to all hostnames and whitelist port 80.

--- api/api/settings.py
+++ api/api/settings.py
@@ -10,23 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a+*)5tmfjcnqea9!^0+50++byy10cc==4aqmokci(l8k$b*%jm'
+SECRET_KEY = os.getenv('SECRET_KEY', 'pl34s3ch4ng3m3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -65,6 +61,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1',
     'http://127.0.0.1:8080',
     'http://localhost:8080',
     'https://accounts.google.com'
