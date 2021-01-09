afa18094e17a891d37cb7102f38b9b7b9b1f530a
Corey <mechtrondev@gmail.com>
2021-01-09 14:29:29 -0800

    Dynamically deliver API hostname via env var API_HOSTNAME

--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,6 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
+import axios from "axios"
 import Vue from 'vue'
 import './plugins/axios'
 import './plugins/bootstrap-vue'
@@ -9,11 +10,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
-Vue.use(infiniteScroll)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const getRuntimeConfig = async () => {
+  const runtimeConfig = await fetch('/config.json')
+  return await runtimeConfig.json()
+}
+
+getRuntimeConfig().then(function(json) {
+  Vue.config.productionTip = false
+  axios.defaults.baseURL = json.API_HOSTNAME
+  Vue.use(infiniteScroll)
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+});
